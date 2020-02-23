bbc2dba72c577804298322551424a7f02cfdf1cb
omid <omdi.ebadi@gmail.com>
2020-02-23 10:00:59 +0330

    activate lcd and create display function

--- display_number/display_number/display_number.c
+++ display_number/display_number/display_number.c
@@ -10,6 +10,9 @@
 #include "library/global.h"
 #include <util/crc16.h>
 #include "library/keypad_ostad.h"
+#include "library/lcd.h"
+
+void lcd_display(uint8_t);
 
 int main(void)
 {
@@ -17,12 +20,17 @@ int main(void)
 	keypadInit();
 	DDRD = 0xff;
 	PORTD = 0; 
+	lcd_init(LCD_DISP_ON_CURSOR);
     while(1)
     {
 		if ( (num = keyScan()) != 255)
 		{
 			PORTD = num;
 		}
-         
     }
+}
+
+void lcd_display(uint8_t)
+{
+	
 }
